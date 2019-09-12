960963120d97bde25cab7aaede3a6e514d7891f5
simoganger <simonsoft7@gmail.com>
2019-09-12 22:50:21 +0100

    Initial commit

---
+++ src/main/java/com/simontech/storemongodb/StoreMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.simontech.storemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMongodbApplication.class, args);
+	}
+
+}
